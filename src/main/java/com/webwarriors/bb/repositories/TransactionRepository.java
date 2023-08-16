2b8904ea5df7de88f14f3156ca5916759df8c08d
Simul <simulbista@gmail.com>
2023-08-16 01:22:43 -0400

    updated user APIs and updated transaction repo

--- src/main/java/com/webwarriors/bb/repositories/TransactionRepository.java
+++ src/main/java/com/webwarriors/bb/repositories/TransactionRepository.java
@@ -11,12 +11,14 @@ import com.webwarriors.bb.models.Transaction;
 @Repository
 public interface TransactionRepository extends MongoRepository<Transaction, String> {
 
-	//find all transactions by uid for given month
-	@Query(value = "{ $and: [ { 'uid': ?0 }, { $expr: { $eq: [ { $month: '$transactionDate' }, ?1 ] } } ] }")
+	//find all transactions(undeleted) by uid for given month
+	@Query(value = "{ $and: [ { 'uid': ?0 }, { 'deleteFlag': false }, { $expr: { $eq: [ { $month: '$transactionDate' }, ?1 ] } } ] }")
 	List<Transaction> findByUidAndMonth(String uid, int monthInNumber);
+
 	
-	//find all transactions by uid for given category
-	@Query(value = "{ $and: [ { 'uid': ?0 }, { 'category': ?1 } ] }")
+	//find all transactions(undeleted) by uid for given category
+	@Query(value = "{ $and: [ { 'uid': ?0 }, { 'category': ?1 }, { 'deleteFlag': false } ] }")
 	List<Transaction> findByUidAndCategory(String uid, String category);
+
 
 }
